aeb4d62ea6d682215b1bf3debe2f0083db345245
zirinisp <info@paz-labs.com>
2017-06-19 15:21:03 +0300

    Added SPM support

---
+++ Sources/PazWorldTides.swift
@@ -0,0 +1,90 @@
+//
+//  PazWorldTides.swift
+//  PazWorldTides
+//
+//  Created by Pantelis Zirinis on 22/11/2016.
+//  Copyright © 2016 paz-labs. All rights reserved.
+//
+
+import Foundation
+#if os(iOS)
+    import CoreLocation
+#endif
+import SwiftyJSON
+
+public typealias TidalSetCompletion = (PazWorldTidesResult<TidalSet>) -> Void
+
+public enum PazWorldTidesResult<T> {
+    case success(T)
+    case error(error: PazWorldTides.RequestError)
+}
+
+public class PazWorldTides {
+    
+    public enum UpdateNotification {
+        case requestSent
+        
+        var name: Notification.Name {
+            switch self {
+            case .requestSent: return Notification.Name("PazWorldTides.requestSent")
+            }
+        }
+    }
+    
+    public enum RequestError: Error {
+        case noResult
+        case notInitialized
+        case urlError
+        case serverError(error: Error)
+        case jSonError(error: Error?)
+        case duplicateRequest
+        case unknown
+    }
+    
+    var apiKey: String
+
+    lazy var urlSession: URLSession = {
+        return URLSession.shared
+    }()
+    
+    public init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+    #if os(iOS)
+    public func tidalSetFor(coordinate: CLLocationCoordinate2D, requestTypes: [WorldTidesRequest.RequestType] = [.heights, .extremes], length: Int = 60*60*24*14, maxCalls: Int = 5, completion: @escaping TidalSetCompletion) {
+        guard let request = WorldTidesRequest(apiKey: apiKey, coordinate: coordinate, requestTypes: requestTypes, length: length, maxCalls: maxCalls) else {
+            return completion(.error(error: .unknown))
+        }
+        self.tidalSetFrom(request: request, completion: completion)
+    }
+    #else
+    public func tidalSetFor(latitude: Double, longitude: Double, requestTypes: [WorldTidesRequest.RequestType] = [.heights, .extremes], length: Int = 60*60*24*14, maxCalls: Int = 5, completion: @escaping TidalSetCompletion) {
+        guard let request = WorldTidesRequest(apiKey: apiKey, latitude: latitude, longitude: longitude, requestTypes: requestTypes, length: length, maxCalls: maxCalls) else {
+            return completion(.error(error: .unknown))
+        }
+        self.tidalSetFrom(request: request, completion: completion)
+    }
+    #endif
+    
+    public func tidalSetFrom(request: WorldTidesRequest, completion: @escaping TidalSetCompletion) {
+        guard let url: URL = URL(string: request.urlString)  else {
+            return completion(PazWorldTidesResult.error(error: RequestError.urlError))
+        }
+        
+        let task = self.urlSession.dataTask(with: url) { (data, response, error) in
+            if let result = data {
+                let json = JSON(data: result)
+                guard let tidalSet = TidalSet(json: json) else {
+                    return completion(.error(error: .jSonError(error: nil)))
+                }
+                return completion(.success(tidalSet))
+            } else {
+                if let letError = error {
+                    return completion(.error(error: .serverError(error: letError)))
+                }
+                return completion(.error(error: .unknown))
+            }
+        }
+        task.resume()
+    }
+}
---
+++ Sources/TidalHeight.swift
@@ -0,0 +1,88 @@
+//
+//  TidalHeight.swift
+//  PazWorldTides
+//
+//  Created by Pantelis Zirinis on 22/11/2016.
+//  Copyright © 2016 paz-labs. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class TidalHeight {
+    /// Date/Time of this extreme (in seconds since the unix epoch).
+    open fileprivate (set) var dt: Int
+    /// Date/Time of this extreme (in ISO 8601 standard date and time format, e.g.: 2016-11-22T07:15+0000 ).
+    open fileprivate (set) var date: Date
+    /// Height (in meters) of the tide.
+    open fileprivate (set) var height: Double
+    
+    public init(dt: Int, date: Date, height: Double) {
+        self.dt = dt
+        self.date = date
+        self.height = height
+    }
+    
+    enum Keys: String {
+        case dt = "dt"
+        case date = "date"
+        case height = "height"
+        case type = "type"
+    }
+    
+    static var dateFormatter: DateFormatter = {
+        let dateFormatter: DateFormatter = DateFormatter()
+        #if !os(Linux)
+            DateFormatter.defaultFormatterBehavior = DateFormatter.Behavior.default
+        #endif
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mmZZZ"
+        return dateFormatter
+    }()
+    
+    public convenience init?(json: JSON) {
+        guard let dateString = json[Keys.date.rawValue].string else {
+            return nil
+        }
+        guard let date: Date = TidalHeight.dateFormatter.date(from: dateString) else {
+            return nil
+        }
+        guard let height: Double = json[Keys.height.rawValue].double, let dt: Int = json[Keys.dt.rawValue].int else {
+            return nil
+        }
+        self.init(dt: dt, date: date, height: height)
+    }
+    
+    public func dictionaryValue() -> [String: JSON] {
+        var dictionary = [String: JSON]()
+        dictionary[Keys.date.rawValue] = JSON(TidalHeight.dateFormatter.string(from: self.date))
+        dictionary[Keys.height.rawValue] = JSON(self.height)
+        dictionary[Keys.dt.rawValue] = JSON(self.dt)
+        return dictionary
+    }
+    
+    public func json() -> JSON {
+        return JSON(self.dictionaryValue())
+    }
+    
+    public class func arrayFrom(heightsJsonArray: [JSON]) -> [TidalHeight] {
+        var tidalArray = [TidalHeight]()
+        for json in heightsJsonArray {
+            if let tidalHeight = TidalHeight(json: json) {
+                tidalArray.append(tidalHeight)
+            } else {
+                print("error could not create tidal height from dictionary:\n\(json)")
+            }
+        }
+        return tidalArray
+    }
+}
+
+public func == (lhs: TidalHeight, rhs: TidalHeight) -> Bool {
+    return lhs.date == rhs.date && lhs.dt == rhs.dt && lhs.height == rhs.height
+}
+
+public func != (lhs: TidalHeight, rhs: TidalHeight) -> Bool {
+    return !(lhs == rhs)
+}
+
