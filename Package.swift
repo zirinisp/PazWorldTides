7ab4abf91e7d0b7cadabe41328cab9fb4bbe8071
zirinisp <info@paz-labs.com>
2017-06-21 08:58:00 +0300

    SwiftyJSON moved to IBM-Swift, as we could not upload to bluemix due to conflict

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 let package = Package(
     name: "PazWorldTides",
     dependencies: [
-    .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3)
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16)
+        // This was causing issues with bluemix, as there were two SwiftyJSON Packages. So we had to move to IBM
+        //.Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3)
     ],
     exclude: ["Makefile", "Package-Builder", "Carthage", "build", "Pods"])
