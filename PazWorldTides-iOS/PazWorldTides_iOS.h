994abca010cc6d82cc3ebb4dc7dc27f2569f4b9d
zirinisp <info@paz-labs.com>
2017-09-22 11:28:07 +0300

    Fixed Target

---
+++ PazWorldTides-iOS/PazWorldTides_iOS.h
@@ -0,0 +1,18 @@
+//
+//  PazWorldTides_iOS.h
+//  PazWorldTides-iOS
+//
+//  Created by Pantelis Zirinis on 22/09/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PazWorldTides_iOS.
+FOUNDATION_EXPORT double PazWorldTides_iOSVersionNumber;
+
+//! Project version string for PazWorldTides_iOS.
+FOUNDATION_EXPORT const unsigned char PazWorldTides_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PazWorldTides_iOS/PublicHeader.h>
+
+
